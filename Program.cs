2ac7634517177314c40104deba084372eae47707
stuartjshepherd <stuart@createstudios.com.au>
2020-03-05 08:59:47 +1100

    first commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Events
+{
+    public class Pub
+    {
+        // onChange property containing all the list of subscribers callback methods
+        public event Action OnChange = delegate { };
+
+        public void Raise()
+        {
+            // invoke OnChange Action
+            OnChange();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Events");
+            Console.WriteLine("An event can be used to provide notifications. You can subscribe to an event if you are interested in those notifications.");
+            Console.WriteLine();
+
+            var p = new Pub();
+
+            // register for OnChange event - Subscriber 1
+            p.OnChange += () => Console.WriteLine("Subscriber 1!");
+
+            // register for OnChange event - Subscriber 2
+            p.OnChange += () => Console.WriteLine("Subscriber 2!");
+
+            // raise the event, after this Raise() method is called all subscribers callback methods will get invoked
+            p.Raise();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# Delegate Example
+An event can be used to provide notifications. You can subscribe to an event if you are interested in those notifications.
+
+## Getting Started
+
+### Prerequisites
+
+[.NET Core 3.1 SDK or later](https://dotnet.microsoft.com/download/dotnet-core/3.1)
